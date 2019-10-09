78d51c25a13c643cfc5bc77b73b03fc37af736fe
Ethan <allric0922@gmail.com>
2019-10-09 19:54:42 -0400

    controller completed will watch for bugs

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -8,4 +8,28 @@ router.get("/", function(req, res) {
     burger.all(function (data) {res.render("index", {burgers:data})
         })
 });
+
+router.post("/api/burgers", function(req, res){
+    burger.create("name", req.body.name, function(data){
+        res.json(data);
+    })
+});
+
+router.put("/api/burgers/:id", function(req, res){
+    burger.update({devoured: req.body.devoured}, "id = " + req.params.id, function(data){
+        res.json(data);
+    })
+});
+
+router.delete("/api/burgers/:id", function(req, res){
+    burger.delete("id = " + req.params.id, function(data){
+        if (data == 0){
+            res.status(404).end();
+        }
+        else {
+            res.status(200).end();
+        }
+    })
+});
+
 module.exports = router;

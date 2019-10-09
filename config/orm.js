e5f9c2e0e1712b03e5a2774c0ab1b2118fa75ea5
Ethan <allric0922@gmail.com>
2019-10-09 18:33:04 -0400

    ORM: update and delete added. CRUD completed

--- config/orm.js
+++ config/orm.js
@@ -10,22 +10,53 @@ function questionMarks(length) {
     return array.toString();
   }
 
+  //used this from the cats example /14-handlebars/activities/17-CatsApp with some var changes to help me understand it.
+  function SQLobject(object) {
+    var arr = [];
+
+    for (var key in object) {
+      var value = object[key];
+
+      if (Object.hasOwnProperty.call(object, key)) {
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        arr.push(key + "=" + value);
+      }
+    }
+    return arr.toString();
+  }
+
 var orm = {
     all: function(table, callback) {
         connection.query("SELECT * FROM " + table + ";", function (err, res){
             if (err) throw err;
 
-            callback(res)
-        })
+            callback(res);
+        });
     },
     create: function(table, columns, values, callback){
         connection.query("INSERT INTO "+ table +
-        "(" + columns.toString() + ") VALUES (" +
+        " (" + columns.toString() + ") VALUES (" +
         questionMarks(values.length) + ");", values, function (err, res){
             if (err) throw err;
 
             callback(res)
-        })
+        });
+    },
+    update: function(table, object, condition, callback){
+        connection.query("UPDATE " + table + " SET " + SQLobject(object) + " WHERE " + condition + ";", function (err, res){
+            if (err) throw err;
+
+            callback(res);
+        });
+    },
+    delete: function(table, condition, callback){
+        connection.query("DELETE FROM " + table + " WHERE " + condition, function(err, res){
+            if (err) throw err;
+
+            callback(res);
+        });
     }
 }
 

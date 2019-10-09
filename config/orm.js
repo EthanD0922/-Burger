75ee99839c86884f1db0be4522674bfb4e9e1860
Ethan <allric0922@gmail.com>
2019-10-09 17:49:07 -0400

    ORM: create and all functions added. Connection: forgot to add password

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,33 @@
 var connection = require("../config/connection.js");
 
+function questionMarks(length) {
+    var array = [];
+  
+    for (var i = 0; i < length; i++) {
+      array.push("?");
+    }
+  
+    return array.toString();
+  }
+
+var orm = {
+    all: function(table, callback) {
+        connection.query("SELECT * FROM " + table + ";", function (err, res){
+            if (err) throw err;
+
+            callback(res)
+        })
+    },
+    create: function(table, columns, values, callback){
+        connection.query("INSERT INTO "+ table +
+        "(" + columns.toString() + ") VALUES (" +
+        questionMarks(values.length) + ");", values, function (err, res){
+            if (err) throw err;
+
+            callback(res)
+        })
+    }
+}
+
 
 module.exports = orm;
